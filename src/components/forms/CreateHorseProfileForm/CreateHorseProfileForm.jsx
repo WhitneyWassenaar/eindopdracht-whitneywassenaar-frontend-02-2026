964067bd927e5242fc6de39cf75c11c1c8f21705
WhitneyWassenaar <whitney-wassenaar@live.nl>
2026-07-11 17:17:06 +0200

    Add horseprofile creation form

---
+++ src/components/forms/CreateHorseProfileForm/CreateHorseProfileForm.jsx
@@ -0,0 +1,141 @@
+import "./CreateHorseProfileForm.css";
+import horseBreeds from "../../../data/horseBreeds.js";
+import coatColors from "../../../data/coatColors.js";
+import Button from "../../ui/Button/Button.jsx";
+
+function CreateHorseProfileForm() {
+    return (
+        <form className="create-horse-profile-form-layout">
+            <fieldset>
+                <legend>Paardengegevens</legend>
+
+
+            <div className="form-row">
+                <label>Naam</label>
+                <input
+                    id={"horse-name"}
+                    type={"text"}
+                    maxLength={20}
+                    placeholder={"Voer naam van paard in"}
+                required/>
+            </div>
+
+            <div className="form-row gender-row">
+                <label>Geslacht</label>
+                <div className="radio-button-container">
+
+                    <label>
+                        <input
+                        id={"mare"}
+                        name={"gender"}
+                        value={"Merrie"}
+                        type={"radio"}
+                        required
+                    />
+                        Merrie
+                    </label>
+
+
+                    <label>
+                        <input
+                        id={"stallion"}
+                        name={"gender"}
+                        value={"Hengst"}
+                        type={"radio"}
+                    />
+                        Hengst
+                    </label>
+
+
+                    <label>
+                        <input
+                        id={"gelding"}
+                        name={"gender"}
+                        value={"Ruin"}
+                        type={"radio"}
+                    />
+                        Ruin
+                    </label>
+                </div>
+            </div>
+
+            <div className="form-row">
+                <label>Geboortedatum</label>
+                <input
+                    id="birth-date"
+                    type="date"
+                    required/>
+            </div>
+
+            <div className="form-row">
+                <label>Ras</label>
+                <select
+                    id="breed"
+                    required
+                >
+                    <option
+                        value="">
+                        Selecteer een ras
+                    </option>
+                    {horseBreeds.map((horseBreed) => (
+                        <option
+                            key={horseBreed}
+                            value={horseBreed}>
+                            {horseBreed}
+                        </option>
+                    ))}
+                </select>
+            </div>
+
+<div className="form-row">
+    <label>Kleur</label>
+    <select
+        id="coatColor"
+        required
+    >
+        <option
+            value="">
+            Selecteer een vachtkleur
+        </option>
+        {coatColors.map((coatColor) => (
+            <option
+                key={coatColor}
+                value={coatColor}>
+                {coatColor}
+            </option>
+        ))}
+    </select>
+</div>
+
+<div className="form-row">
+    <label>Eigenaar</label>
+    <select disabled>
+        <option>
+            Eerst contactpersoon aanmaken!
+        </option>
+    </select>
+
+</div>
+
+<div className="form-row">
+    <label>Foto</label>
+    <p>Upload een afbeelding:</p>
+    <input
+    id="horse-photo"
+    type="file"
+    accept="image/*"/>
+</div>
+
+            <Button type={"submit"}>Paardenprofiel aanmaken</Button>
+            </fieldset>
+        </form>
+
+    );
+}
+
+export default CreateHorseProfileForm;
+
+// Foto moet optioneel zijn, niet iedereen heeft gelijk een foto om up te loaden
+// useState toevoegen voor image preview
+// in frontend en backend geboortedautm controle uitvoeren
+// conditioneel renderen van de paardenprofiel aanmaak pagina/component
