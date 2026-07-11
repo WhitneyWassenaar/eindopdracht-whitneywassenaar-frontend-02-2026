b95d2d7e9fd8636b8be8b1d7158644857e56ad18
WhitneyWassenaar <whitney-wassenaar@live.nl>
2026-07-11 17:18:52 +0200

    Add icon, avatar and data

---
+++ src/data/horseBreeds.js
@@ -0,0 +1,20 @@
+const horseBreeds = [
+    "Kruising",
+    "American Paint Horse",
+    "Andalusiër",
+    "Appaloosa",
+    "Arabisch Volbloed",
+    "Engels Volbloed",
+    "Fjord",
+    "Fries",
+    "Franse Draver",
+    "Haflinger",
+    "Hannoveraan",
+    "Ijslander",
+    "KWPN",
+    "NRPS",
+    "Quarter Horse",
+    "Tinker"
+];
+
+export default horseBreeds;
