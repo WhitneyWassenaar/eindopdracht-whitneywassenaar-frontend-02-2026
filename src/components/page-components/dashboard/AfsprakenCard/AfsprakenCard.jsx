c5cbcddf88f33e5ea7a3adc5591873729221deec
WhitneyWassenaar <whitney-wassenaar@live.nl>
2026-07-11 17:09:25 +0200

    Add dashboardcard variants

---
+++ src/components/page-components/dashboard/AfsprakenCard/AfsprakenCard.jsx
@@ -0,0 +1,17 @@
+import DashboardCard from "../DashboardCard/DashboardCard.jsx";
+
+function AfsprakenCard() {
+    return (
+        <DashboardCard
+            title={"Afspraken"}
+        >
+            <ul className="afspraken-list">
+                <li>Aankomende afspraken:</li>
+                <li>Behandelingen:</li>
+                <li>Trainingen:</li>
+            </ul>
+        </DashboardCard>
+    );
+}
+
+export default AfsprakenCard;
---
+++ src/components/page-components/dashboard/PaardenverdelingCard/Paardenverdeling.jsx
@@ -0,0 +1,27 @@
+import DashboardCard from "../DashboardCard/DashboardCard.jsx";
+
+function PaardenverdelingCard() {
+
+    return (
+        <DashboardCard
+            title={"Paardenverdeling"}
+        >
+            <ul className="paardenverdeling-list">
+                <li>
+                    <span>Stal capaciteit: </span>
+                    <strong>10</strong>
+                </li>
+                <li>
+                    <span>Paarden op stal: </span>
+                    <strong>10</strong>
+                </li>
+                <li>
+                    <span>Paarden op de wei: </span>
+                    <strong>10</strong>
+                </li>
+            </ul>
+        </DashboardCard>
+    );
+}
+
+export default PaardenverdelingCard
