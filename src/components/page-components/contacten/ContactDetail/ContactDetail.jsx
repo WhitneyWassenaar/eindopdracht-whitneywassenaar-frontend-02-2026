8374e594cabf71449cc4562dd165dc26b2b883f7
WhitneyWassenaar <whitney-wassenaar@live.nl>
2026-07-14 16:55:56 +0200

    Filter related horses based on  contact.id

--- src/components/page-components/contacten/ContactDetail/ContactDetail.jsx
+++ src/components/page-components/contacten/ContactDetail/ContactDetail.jsx
@@ -1,11 +1,21 @@
 import './ContactDetail.css';
 import ContactDetailMainInfo from "../ContactDetailMainInfo/ContactDetailMainInfo.jsx";
+import {getHorses} from "../../../../data/horseApi.js";
 
 function ContactDetail({contact}) {
     console.log("ContactDetail:", contact);
+
+    const relatedHorses = getHorses.filter((horseDetail) =>
+    horseDetail.ownerId === contact.id ||
+    horseDetail.careTakerId === contact.id ||
+    horseDetail.trainerId === contact.id);
+
     return (
         <div className="contact-detail-container">
-            <ContactDetailMainInfo contact={contact}/>
+            <ContactDetailMainInfo
+                contact={contact}
+                horses = {[]}
+            />
         </div>
 
     );
