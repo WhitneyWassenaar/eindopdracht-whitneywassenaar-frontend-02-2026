15f0be9cf7ba16342e237c62bede0d45d590c4c3
WhitneyWassenaar <whitney-wassenaar@live.nl>
2026-07-17 08:53:12 +0200

    Fetch caretasks for current user

--- src/pages/dashboardlayout/Zorgtaken/Zorgtaken.jsx
+++ src/pages/dashboardlayout/Zorgtaken/Zorgtaken.jsx
@@ -1,11 +1,53 @@
+// React
+import {useContext, useEffect, useState} from "react";
+
 // CSS
 import './Zorgtaken.css'
+import api from "../../../api/axios.js";
+import {AuthContext} from "../../../components/authentication/context/AuthContext.jsx";
+
 
 function Zorgtaken() {
+    const {token,user} = useContext(AuthContext);
+    const [careTasks, setCareTasks] = useState([]);
+
+    useEffect(() => {
+        if(!token || !user) return;
+
+        async function getCareTasks() {
+            try {
+                const careTasksData = await api.get(`/users/${user.id}/careTasks`,
+                    {
+                        headers: {
+                            Authorization: `Bearer ${token}`
+                        }
+                    });
+
+                setCareTasks(careTasksData.data);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+
+        getCareTasks();
+
+    },[token,user])
+
     return (
         <>
             <div className="dashboard-page">
                 <h1>Zorgtaken</h1>
+                {careTasks.length === 0 ? (
+                    <p>Geen zorgtaken gevonden</p>
+                ):(
+                    careTasks.map(task => (
+                            <div key={task.id}>
+                                <h3>{task.title}</h3>
+                                <p>{task.description}</p>
+                            </div>
+                        ))
+                )}
+
             </div>
         </>
     );
