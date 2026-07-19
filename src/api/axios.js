cc2b7ba1544f4a8c083228d7aff1933691d37df8
WhitneyWassenaar <whitney-wassenaar@live.nl>
2026-07-19 22:08:27 +0200

    Fix projectid

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,15 +1,13 @@
 // Axios
 import axios from "axios";
 
-// Data
-import projectId from "../data/projectId.js";
 
 const api = axios.create(
     {
         baseURL: "https://novi-backend-api-wgsgz.ondigitalocean.app/api",
         headers: {
             "Content-Type": "application/json",
-            "novi-education-project-id": projectId
+            "novi-education-project-id": import.meta.env.VITE_PROJECT_ID
         }
     }
 );
